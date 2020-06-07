42095ca6ca0641a578502022dad9d36a8a632580
Jairo <jairopereira_dias@hotmail.com>
2020-06-07 14:57:12 -0300

    criando a model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,7 +1,14 @@
-import { Model } from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
   static init(sequelize) {
-    super.init();
+    super.init({
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
+      provider: Sequelize.BOOLEAN,
+    });
   }
 }
+
+export default User;
