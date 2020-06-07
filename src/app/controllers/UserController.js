d50edc9b46ae45d0021b6688137006b142e4efad
Jairo <jairopereira_dias@hotmail.com>
2020-06-07 15:20:20 -0300

    verificando a existencia de usuarios

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,17 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
     const user = await User.create(req.body);
-    return res.json();
+
+    return res.json(user);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Jairo Dias',
-    email: 'jairopereira_dias@hotmail.com',
-    password_hash: '514684849',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
